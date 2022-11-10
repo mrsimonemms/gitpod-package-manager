78dca1f7c8d641f114d23b24b69d3a0652eb9015
Simon Emms <simon@simonemms.com>
2022-11-10 21:04:36 +0000

    feat(gpm): initial commit and scoped the functions

---
+++ gpm.sh
@@ -0,0 +1,148 @@
+#!/bin/bash
+
+################################################################################
+# ▛▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▜ #
+# ▌ Copyright © 2022 Simon Emms <simon@simonemms.com>                        ▐ #
+# ▌                                                                          ▐ #
+# ▌ Licensed under the Apache License, Version 2.0 (the "License");          ▐ #
+# ▌ you may not use this file except in compliance with the License.         ▐ #
+# ▌ You may obtain a copy of the License at                                  ▐ #
+# ▌                                                                          ▐ #
+# ▌     http://www.apache.org/licenses/LICENSE-2.0                           ▐ #
+# ▌                                                                          ▐ #
+# ▌ Unless required by applicable law or agreed to in writing, software      ▐ #
+# ▌ distributed under the License is distributed on an "AS IS" BASIS,        ▐ #
+# ▌ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. ▐ #
+# ▌ See the License for the specific language governing permissions and      ▐ #
+# ▌ limitations under the License.                                           ▐ #
+# ▙▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▟ #
+#                                                                              #
+# ▛▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▜ #
+# ▌ Source code: https://github.com/MrSimonEmms/gitpod-package-manager       ▐ #
+# ▌ Install URL: https://gpm.simonemms.com                                   ▐ #
+# ▌ Me:          https://www.simonemms.com                                   ▐ #
+# ▌ Gitpod:      https://www.gitpod.io                                       ▐ #
+# ▙▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▟ #
+################################################################################
+
+set -euo pipefail
+
+#################
+# ▛▀▀▀▀▀▀▀▀▀▀▀▜ #
+# ▌ Variables ▐ #
+# ▙▄▄▄▄▄▄▄▄▄▄▄▟ #
+#################
+
+USE_REMOTE_REPO=0
+if [ -z "${BASH_SOURCE:-}" ]; then
+  CMD="${CMD:-init}"
+  USE_REMOTE_REPO=1
+else
+  CMD="${1:-}"
+fi
+
+GPM_REPO_URL="${GPM_REPO_URL:-https://raw.githubusercontent.com/MrSimonEmms/gitpod-package-manager/main}"
+TARGET_INSTALL_PATH="/usr/local/bin"
+
+#########################
+# ▛▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▜ #
+# ▌ Private Functions ▐ #
+# ▙▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▟ #
+#########################
+
+# Generate help text for commands
+_help() {
+  text="${1}"
+
+  if [[ "${*}" =~ (--help|-h)(\b|$) ]]; then
+    echo "${text}"
+    exit
+  fi
+}
+
+########################
+# ▛▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▜ #
+# ▌ Public Functions ▐ #
+# ▙▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▟ #
+########################
+
+init() {
+  help=$(cat << EOF
+Installs Gitpod Package Manager to your Gitpod workspace
+
+Packages are pulled from the GPM_REPO_URL envvar, currently set to:
+  ${GPM_REPO_URL}
+
+Usage:
+  init
+
+Flags:
+  -h, --help       Display help
+EOF
+)
+  _help "${help}" "${@}"
+
+  sudo curl -sfL "${GPM_REPO_URL}/gpm.sh" -o "${TARGET_INSTALL_PATH}/gpm"
+  sudo chmod +x "${TARGET_INSTALL_PATH}/gpm"
+
+  cat << EOF
+Gitpod Package Manager installed to ${TARGET_INSTALL_PATH}/gpm
+
+To install packages, run gpm install <package1> <package2>...
+EOF
+}
+
+install() {
+  help=$(cat << EOF
+Installs packages to your Gitpod workspace
+
+Packages are pulled from the GPM_REPO_URL envvar, currently set to:
+  ${GPM_REPO_URL}
+
+Usage:
+  install <package1> <package2>...
+
+Flags:
+  -h, --help       Display help
+EOF
+)
+  _help "${help}" "${@}"
+}
+
+#######################
+# ▛▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▜ #
+# ▌ Public Commands ▐ #
+# ▙▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▟ #
+#######################
+
+case "${CMD}" in
+  init )
+    if [ "${USE_REMOTE_REPO}" -eq 0 ]; then
+      shift 1
+    fi
+
+    init "${@}"
+    ;;
+  install )
+    if [ "${USE_REMOTE_REPO}" -eq 0 ]; then
+      shift 1
+    fi
+
+    install "${@}"
+    ;;
+  * )
+    cat << EOF
+A package manager for Gitpod workspaces
+
+Usage:
+  gpm [command]
+
+Available commands:
+  init              Installs Gitpod Package Manager to your Gitpod workspace
+  install           Installs packages to your Gitpod workspace
+
+Flags:
+  -h, --help        Display help
+EOF
+    ;;
+esac
