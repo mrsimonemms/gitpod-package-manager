b4aa03b9cc43536c0e8716f7e6cdb7af1fa0a89b
Simon Emms <simon@simonemms.com>
2022-11-11 11:36:19 +0000

    feat(packages): add terraform command

---
+++ packages/terraform.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -euo pipefail
+
+gpm_terraform_install() {
+  wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+  echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list > /dev/null
+  sudo apt update
+  sudo apt install -y terraform
+  terraform version
+}
