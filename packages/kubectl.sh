5bb99c6e15040eb5bd4043f295ba0c061410add4
Simon Emms <simon@simonemms.com>
2022-11-13 21:16:08 +0000

    feat(gpm): add ability to download versions of binaries

--- packages/kubectl.sh
+++ packages/kubectl.sh
@@ -3,7 +3,13 @@
 set -euo pipefail
 
 gpm_kubectl_install() {
-  sudo curl -s -L "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" -o /usr/local/bin/kubectl
+  version="${1}"
+
+  if [ "${version}" = "latest" ]; then
+    sudo curl -sSfL "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" -o /usr/local/bin/kubectl
+  else
+    sudo curl -sSfL "https://dl.k8s.io/release/v${version}/bin/linux/amd64/kubectl" -o /usr/local/bin/kubectl
+  fi
   sudo chmod +x /usr/local/bin/kubectl
   kubectl completion bash | sudo tee -a /etc/bash_completion.d/kubectl > /dev/null
   kubectl version --client -o json
